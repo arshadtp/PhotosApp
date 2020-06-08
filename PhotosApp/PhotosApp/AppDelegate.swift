064a00ba1b6a63c849bd411bdbdfb6cc85d97bfb
Arshad <arshad@qburst.com>
2020-06-08 20:24:25 +0530

    Adding base project

---
+++ PhotosApp/PhotosApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotosApp
+//
+//  Created by Arshad T P on 6/8/20.
+//  Copyright © 2020 Ab'initio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
